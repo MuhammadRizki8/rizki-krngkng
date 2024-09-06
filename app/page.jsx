0a0e3a4f42b32016ca32aa845c59c9cf28b81413
MuhammadRizki8 <muhammadrizki@upi.edu>
2024-09-06 23:54:13 +0700

    home page layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
 import { Button } from '@/components/ui/button';
 import { FiDownload } from 'react-icons/fi';
 import Social from '@/components/ui/Social';
-
+import Photo from '@/components/Photo';
+import Stats from '@/components/Stats';
 const Home = () => {
   return (
     <section className="h-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 space-y-8 xl:space-y-0">
           {/* Text */}
-          <div className="text-center xl:text-left space-y-4 xl:space-y-6">
+          <div className="text-center xl:text-left space-y-4 xl:space-y-6 order-2 xl:order-none">
             <span className="block text-lg text-gray-500">Computer Science Student</span>
             <h1 className="text-4xl sm:text-5xl xl:text-7xl font-semibold leading-tight">
               Hello, I&apos;m <br />
@@ -25,13 +26,12 @@ const Home = () => {
           </div>
 
           {/* Photo */}
-          <div className="mt-8 xl:mt-0">
-            <div className="w-48 h-48 sm:w-56 sm:h-56 md:w-64 md:h-64 bg-gray-200 rounded-full flex items-center justify-center">
-              <span>Photo</span>
-            </div>
+          <div className="order-1 xl:order-none mb-8 py-8 xl:mb-0">
+            <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
